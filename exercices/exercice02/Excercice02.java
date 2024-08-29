b137360b63e338ecb030df454793808d9c78d7d6
Nolhan <Nolhan.VandenBroeck@studentfr.ch>
2024-08-29 11:46:15 +0200

    Message

---
+++ exercices/exercice02/Excercice02.java
@@ -0,0 +1,12 @@
+package exercices.exercice02;
+
+public class Excercice02 {
+    public static void main(String[] args) {
+        System.out.println("Bonjour tout le monde!");
+byte age = 16;
+
+
+
+    }
+
+}
---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,13 @@
+package exercices.exercice03;
+
+public class TypesDeBase {
+    public static void main(String[] args) {
+        int age = 16;
+        String nom = "Nolhan Vanden Broeck";
+        System.out.println("Je m'apelle "+nom + " et j'ai " + age + " ans");
+        
+System.out.println("Dans 10 ans, j'aurai"
+    } 
+
+
+}
