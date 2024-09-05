dda173f06090f433c5d616b6eb4f739b2d1f12ce
Nolhan <Nolhan.VandenBroeck@studentfr.ch>
2024-09-05 09:29:45 +0200

    ex.cond1

---
+++ exercices/exercice05/ExerciceCondition1.java
@@ -0,0 +1,15 @@
+package exercices.exercice05;
+
+public class ExerciceCondition1 {
+
+    public static void main(String[] args) {
+        int valeurTester = -9;
+        int zéro = 0;
+        if (valeurTester > zéro) {
+            System.out.println("Le nombre est positif");
+        } else {
+            System.out.println("Le nombre est négaif");
+        }
+
+    }
+}
