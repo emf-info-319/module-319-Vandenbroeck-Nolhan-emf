82f0601fa675ae3b80e7d44f93d5a493767f3acb
Nolhan <Nolhan.VandenBroeck@studentfr.ch>
2024-09-05 08:37:44 +0200

    ex.4

--- exercices/exercice03/TypesDeBase.java
+++ exercices/exercice03/TypesDeBase.java
@@ -6,7 +6,7 @@ public class TypesDeBase {
         String nom = "Nolhan Vanden Broeck";
         System.out.println("Je m'apelle "+nom + " et j'ai " + age + " ans");
         
-System.out.println("Dans 10 ans, j'aurai"
+System.out.println("Dans 10 ans, j'aurai");
     } 
 
 
