c37c7a7187082c408fb6b3ba8e20aab99586ca33
Nolhan <Nolhan.VandenBroeck@studentfr.ch>
2024-09-05 09:49:52 +0200

    ex.cond2

--- exercices/exercice05/ExerciceCondition2.java
+++ exercices/exercice05/ExerciceCondition2.java
@@ -1,5 +1,9 @@
 package exercices.exercice05;
 
-public class ExerciceCondition2 {
+public class ExerciceCondition2 {public static void main(String[] args) {
+    int valeurATester = 8;
+    if (valeurATester % 2 == 0) {System.out.println("Le nombre est est paire");}
+    else {System.out.println("Le nombre est impaire" );}
+ }
     
 }
