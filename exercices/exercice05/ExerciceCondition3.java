c419edcbd30173c14756cba78ec689a76b2ae8b5
Nolhan <Nolhan.VandenBroeck@studentfr.ch>
2024-09-05 09:54:38 +0200

    ex.cond3

--- exercices/exercice05/ExerciceCondition3.java
+++ exercices/exercice05/ExerciceCondition3.java
@@ -1,5 +1,12 @@
 package exercices.exercice05;
 
-public class ExerciceCondition3 {
-    
+public class ExerciceCondition3 {public static void main(String[] args) {
+    int valeur1 = 9;
+    int valeur2 = 5;
+if (valeur1* valeur2 > 0) {System.out.println("Le nombre est positif");}
+else {System.out.println("Le nombre est négatif");}
+
+
+
+}
 }
