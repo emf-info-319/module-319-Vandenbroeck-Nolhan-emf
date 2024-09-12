a017538fbac33118fda65cad09767a3eee9506c8
Nolhan <Nolhan.VandenBroeck@studentfr.ch>
2024-09-12 12:54:38 +0200

    oe

---
+++ devoirs/devoir02/devoir02.java
@@ -0,0 +1,15 @@
+package devoirs.devoir02;
+
+public class devoir02 {
+    public final static int CAPACITE_RESERVOIR_A=3;
+    public final static int CAPACITE_RESERVOIR_B=5;
+    public static void main(String[] args) {
+int nombre;
+int reservoirA=0;
+int reservoirB=0;
+int remplissage = ( int ) ( Math.random() * ( 8 - 1 + 1 ) ) + 1;
+System.out.println("Nombre de litres à remplir = " + remplissage);
+for (remplissage = nombre; remplissage == CAPACITE_RESERVOIR_A; reservoirA++);
+    }
+
+}
---
+++ revisions/onenote01.java
@@ -0,0 +1,18 @@
+package revisions;
+
+/**
+ * onenote01
+ */
+public class onenote01 {public static void main(String[] args)
+    
+     {
+        for (int i = (i+1) )
+    
+    
+    }
+    
+}
+
+    
+
+    
