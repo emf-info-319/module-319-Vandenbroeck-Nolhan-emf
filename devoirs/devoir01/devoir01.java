82ff013830fa8c159f1a0bae0140e4e1f317dc75
me <Nolhan.VandenBroeck@studentfr.ch>
2024-09-25 00:55:26 +0200

    devoir03

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -1,32 +1,19 @@
 package devoirs.devoir01;
 
-public class Devoir01 {
-    public static void main(String[] args) {
-        int monAge = 10;
-        boolean estMajeur;
-        if (monAge > 20) {
-            estMajeur = true;
-        } else {
-            estMajeur = false;
-        }
-        if (estMajeur) {
-            System.out.println("Je suis Majeur");
-        } else {
-            System.out.println("Je suis mineur");
-        }
-        if (monAge > 18) {
-            System.out.println("J'ai le droit de voter");
-        } else {
-            System.out.println("Je n'ai pas le droit de voter");
-        }
+public class devoir01 {public static void main(String[] args) {
+    int monAge = 10;
+    String estMajeur1 = "Majeur";
+    if (monAge > 20) {System.out.println("Je suis " + estMajeur1);}
+        if ( monAge < 20) {System.out.println("Je ne suis pas encore " +estMajeur1);}
+        if (monAge > 15) {System.out.println("Je peux conduire un motocycle ");}
+            else {System.out.println("Je ne peux tojours pas conduire de motocycle");}
+        if (monAge > 18) {System.out.println("J'ai le droit de voter");}
+        else {System.out.println("Je n'ai pas le droit de voter");}
         System.out.println("Je m'appelle Nolhan Vanden Broeck");
         System.out.println("Mon âge est de " + monAge + " ans");
         String formation = "Informatique";
         String Informatique = "Informatique";
-        if (formation == Informatique) {
-            System.out.println("Je suis apprenti en informatique");
-        } else {
-            System.out.println("Je suis apprenti en industrie");
-        }
+        if (formation == Informatique) {System.out.println("Je suis apprenti en informatique");}
+        else {System.out.println("Je suis apprenti en industrie");}
     }
 }
---
+++ devoirs/devoir03/devoir03.java
@@ -0,0 +1,88 @@
+package devoirs.devoir03;
+
+public class devoir03 {
+
+    public static void main(String[] args) {
+    int jour = 77;
+    int mois = 1;
+    int annee = 2024;
+    int a = 4;
+    if (annee <= 9999){
+        if (jour< 31) {
+    if (mois == 1){System.out.println("Le " + jour   + mois   + annee  + jour);}}
+}
+switch (mois) {
+    case 2:
+    if (annee <= 9999){
+        if (jour< 29) {
+System.out.println("Le " + jour   + mois   + annee  + jour+31);}}
+break;
+case 3:
+if (jour< 31) {
+if (annee <= 9999){
+if (annee % a == 0){System.out.println("Le " + jour   + mois   + annee  + (jour+60));}
+else {System.out.println("Le " + jour   + mois   + annee  + (jour+59));}}}
+break;
+case 4:
+if (annee <= 9999){
+    if (jour< 30) {
+if (annee % a == 0){System.out.println("Le " + jour   + mois   + annee  + (jour+91));}
+else {System.out.println("Le " + jour   + mois   + annee  + (jour+90));}}}
+break;
+case 5:
+if (annee <= 9999){
+    if (jour< 31) {
+if (annee % a == 0){System.out.println("Le " + jour   + mois   + annee  + (jour+121));}
+else {System.out.println("Le " + jour   + mois   + annee  + (jour+120));}}}
+break;
+case 6:
+if (annee <= 9999){
+    if (jour< 30) {
+if (annee % a == 0){System.out.println("Le " + jour   + mois   + annee  + (jour+152));}
+else {System.out.println("Le " + jour   + mois   + annee  + (jour+151));}}}
+break;
+case 7:
+if (annee <= 9999){
+    if (jour< 31) {
+if (annee % a == 0){System.out.println("Le " + jour   + mois   + annee  + (jour+182));}
+else {System.out.println("Le " + jour   + mois   + annee  + (jour+181));}}}
+break;
+case 8:
+if (annee <= 9999){
+    if (jour< 31) {
+if (annee % a == 0){System.out.println("Le " + jour   + mois   + annee  + (jour+213));}
+else {System.out.println("Le " + jour   + mois   + annee  + (jour+212));}}}
+break;
+case 9:
+if (annee <= 9999){
+    if (jour< 30) {
+if (annee % a == 0){System.out.println("Le " + jour   + mois   + annee  + (jour+244));}
+else {System.out.println("Le " + jour   + mois   + annee  + (jour+243));}}}
+break;
+case 10:
+if (annee <= 9999){
+    if (jour< 31) {
+if (annee % a == 0){System.out.println("Le " + jour   + mois   + annee  + (jour+274));}
+else {System.out.println("Le " + jour   + mois   + annee  + (jour+273));}}}
+break;
+case 11:
+if (annee <= 9999){
+    if (jour< 30) {
+if (annee % a == 0){System.out.println("Le " + jour   + mois   + annee  + (jour+305));}
+else {System.out.println("Le " + jour   + mois   + annee  + (jour+304));}}}
+break;
+case 12:
+if (annee <= 9999){
+    if (jour< 31) {
+if (annee % a == 0){System.out.println("Le " + jour   + mois   + annee  + (jour+335));}
+else {System.out.println("Le " + jour   + mois   + annee  + (jour+334));}}}
+break;
+
+
+        
+
+
+    
+        
+}}}
+
